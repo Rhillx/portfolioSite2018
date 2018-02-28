1ad2bc643e13a214d98f8fd63fc8d2f81b54d123
Richard Hill III <rhillz718@gmail.com>
2018-02-27 20:55:27 -0500

    initial commit

---
+++ JS/index.js
@@ -0,0 +1,24 @@
+(() => {
+
+
+const myName = document.querySelector(".heading1");
+
+
+myName.addEventListener("mouseover", (e) =>{
+   myName.innerHTML = "RHILLX";
+});
+
+myName.addEventListener("mouseleave", (e)=>{
+    myName.innerHTML = "Richard Hill";
+})
+
+
+
+
+
+
+
+
+
+
+})();
