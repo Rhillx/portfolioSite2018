ebd6672d54766cf8ad0980db084bbbb59cec9d59
Richard Hill III <rhillz718@gmail.com>
2018-05-13 17:18:24 -0400

    changed to under contruction temporarily

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,19 @@
-(() => {
+(()=> {
 
+// bio js
 
+let current = 1; //keeps track of the current div
+const height = $('.roles').height(); //the height of the roles div
+const numberDivs = $('.roles').children().length; //the number of children of the roles div
+const first = $('.roles div:nth-child(1)'); //the first div nested in roles div
+setInterval(()=>  {
+    const number = current * -height;
+    first.css('margin-top', number + 'px');
+    if (current === numberDivs) {
+        first.css('margin-top', '0px');
+        current = 1;
+    } else current++;
+}, 2000);
 
 
 
---
+++ JS/nav.js
@@ -0,0 +1,20 @@
+(()=>{
+
+
+$(".navT").on("click", function(){
+  $(this).toggleClass("active");
+  $("#menu").toggleClass("open");
+  $(".content").toggleClass("shift");
+  
+  
+})
+
+
+
+
+
+
+
+
+
+})();
